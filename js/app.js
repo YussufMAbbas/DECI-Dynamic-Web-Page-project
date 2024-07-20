00ec1b0a5d0ed851dc381dd2aa792c7bd8d2312c
YussufMAbbas <118044850+YussufMAbbas@users.noreply.github.com>
2024-07-20 12:03:29 +0300

    Add smooth scrolling

--- js/app.js
+++ js/app.js
@@ -5,11 +5,16 @@ const navbar = document.querySelector("#navbar__list");
 sections.forEach((section) => {
     let navLink = document.createElement("li");
     navLink.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
-    navbar.appendChild(navLink)
+    navbar.appendChild(navLink);
 });
 
 /** TODO: Add smooth scrolling **/
-
+navbar.addEventListener("click", (event) => {
+    if (event.target.nodeName === "A") {
+        event.preventDefault();
+        document.querySelector(event.target.getAttribute("href")).scrollIntoView({ behavior:"smooth" });
+    }
+});
 /** TODO: Add an active state **/
 
 /** TODO: Add a comment form **/
